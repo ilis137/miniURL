18bbe238fbc8dbf20d3305df4cc2cfc19e9c195f
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-12-12 07:04:20 +0530

    fix the connection error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var MONGO_DB = process.env.MONGO_DB;
 
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017")
+mongoose.connect("mongodb://localhost:27017/ShortURL", { useNewUrlParser: true })
 
 URLshortner(app)
 
